d1689419724039f21294cecb7e00b06d3f12f4e3
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-09-25 12:39:36 +0300

    restore hyprland config properly

--- restore.sh
+++ restore.sh
@@ -8,7 +8,7 @@ done
 
 mkdir "$HOME/.config/"
 for filename in alacritty compton.conf gtk-3.0 mimeapps.list mpv vifm qutebrowser kitty\
-	rofi sxhkd sxiv user-dirs.dirs zathura nushell wezterm; do
+	rofi sxhkd sxiv user-dirs.dirs zathura nushell wezterm hypr; do
 	ln -sf "$HOME/work/otherdots/.config/$filename" "$HOME/.config/"
 done
 
