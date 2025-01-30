6010beb6a2d4388a338a85e58c8dd28c0ea09393
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-01-30 08:19:40 +0200

    more keybindings

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -8,13 +8,16 @@
 (setf lem/prompt-window::*fill-width* t)
 
 ;; C-h is initially bound to delete-previous-char
-(define-keys *global-keymap*
-  ("C-h b" 'describe-bindings)
-  ("C-h k" 'describe-key)
-  ("C-h a" 'lem-lisp-mode:lisp-apropos)
-  ("C-h c" 'apropos-command)
-  ("C-h p" 'lem-lisp-mode:lisp-apropos-package)
-  ("C-h f" 'lem-lisp-mode:lisp-describe-symbol))
+(defvar *help-keymap*
+  (make-keymap :name '*my-help-keymap*)
+  "keymap for help commands.")
+(define-keys *help-keymap*
+  ("b" 'describe-bindings)
+  ("k" 'describe-key)
+  ("a" 'lem-lisp-mode:lisp-apropos)
+  ("c" 'apropos-command)
+  ("p" 'lem-lisp-mode:lisp-apropos-package)
+  ("f" 'lem-lisp-mode:lisp-describe-symbol))
 
 (defvar *frame-keymap*
   (make-keymap :name '*frame-multiplexer-keymap*)
@@ -24,15 +27,40 @@
 (define-key *frame-keymap* "p" 'frame-multiplexer-prev)
 (define-key *frame-keymap* "n" 'frame-multiplexer-next)
 (define-key *frame-keymap* "r" 'frame-mulitplexer-rename)
-(define-key *global-keymap* "C-z" *frame-keymap*)
+
+(defvar *window-keymap*
+  (make-keymap :name '*my-window-keymap*)
+  "keymap for window related commands.")
+(define-keys *window-keymap*
+  ("l" 'lem-vi-mode/binds::vi-window-move-right)
+  ("h" 'lem-vi-mode/binds::vi-window-move-left)
+  ("j" 'lem-vi-mode/binds::vi-window-move-down)
+  ("k" 'lem-vi-mode/binds::vi-window-move-up)
+  ("s" 'lem-vi-mode/binds::vi-window-split-vertically)
+  ("c" 'lem-vi-mode/binds::vi-close)
+  ("o" 'lem-vi-mode/binds::delete-other-windows)
+  )
 
 ;; relative line numbers
 (lem/line-numbers:toggle-line-numbers)
 (setf lem/line-numbers:*relative-line* t)
 
+;; spc spc to run commands
 (define-key lem-vi-mode:*normal-keymap*
   "Space Space"
   'execute-command)
+;; s to save
+(define-key lem-vi-mode:*normal-keymap*
+  "s"
+  'save-current-buffer)
+;; spc-h for help keys
+;; (define-key *global-keymap* "C-z" *frame-keymap*)
+(define-key lem-vi-mode:*normal-keymap*
+  "Space h"
+  *help-keymap*)
+(define-key lem-vi-mode:*normal-keymap*
+  "Space w"
+  *window-keymap*)
 
 (defun led-key (key command)
   (define-key lem-vi-mode:*normal-keymap*
@@ -47,4 +75,8 @@
           (lambda ()
             (lem/completion-mode:completion-end)))
 
+;; keys to find files
 (led-key "f f" 'find-file)
+(led-key "e" (lambda () (find-file "/home/mahmooz/.lem/init.lisp")))
+
+(led-key "x" 'lem-lisp-mode/eval::lisp-eval-defun)
