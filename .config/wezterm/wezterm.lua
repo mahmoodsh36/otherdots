8ca1aabb249d69d91d358f8c1a51f8456e96c06b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-02-15 18:57:43 +0200

    enable tmux-like persistent session functionality, add more pane navigation keys

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'AdventureTime'
 config.color_scheme = 'Batman'
 config.enable_scroll_bar = true
 
@@ -11,6 +10,16 @@ config.ssh_domains = {
     name = 'mahmooz2',
     remote_address = 'mahmooz2',
     username = 'mahmooz',
+  },
+}
+
+config.font = wezterm.font('iosevka')
+config.font_size = 10.0
+
+-- for persistent multiplexing sessions (like tmux)
+config.unix_domains = {
+  {
+    name = 'unix',
   },
 }
 
@@ -24,8 +33,16 @@ config.keys = {
   {key="7", mods="ALT", action=wezterm.action{ActivateTab=6}},
   {key="8", mods="ALT", action=wezterm.action{ActivateTab=7}},
   {key="9", mods="ALT", action=wezterm.action{ActivateTab=8}},
+
   {key="s", mods="ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
   {key="v", mods="ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+
+  {key="h", mods="ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
+  {key="l", mods="ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
+  {key="k", mods="ALT", action=wezterm.action{ActivatePaneDirection="Up"}},
+  {key="j", mods="ALT", action=wezterm.action{ActivatePaneDirection="Down"}},
+
+  {key="t", mods="ALT", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
 }
 
 return config
